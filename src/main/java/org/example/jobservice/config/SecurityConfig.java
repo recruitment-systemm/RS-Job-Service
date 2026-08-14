6132dbd1f29bfdb9ad67ae8ab2ebfbc41e5d3e80
ahmedali109 <ahmedalinaguib33@gmail.com>
2026-08-14 19:41:51 +0300

    Add Security Configurations

---
+++ src/main/java/org/example/jobservice/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package org.example.jobservice.config;
+
+import lombok.RequiredArgsConstructor;
+import org.example.jobservice.filter.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/v1/jobs", "/api/v1/jobs/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/jobs").hasRole("HR")
+                        .requestMatchers(HttpMethod.PUT, "/api/v1/jobs/**").hasRole("HR")
+                        .requestMatchers(HttpMethod.PATCH, "/api/v1/jobs/**").hasRole("HR")
+                        .requestMatchers(HttpMethod.DELETE, "/api/v1/jobs/**").hasRole("HR")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
